973cf416d9e6f0c2a0d2cc3a1b6c5e6fbb902fb4
Tomas Nevrlka <tomas@nevrlka.com>
2024-12-03 01:37:54 +0100

    add solution to first part of day 2

---
+++ 02/nocap.py
@@ -0,0 +1,32 @@
+lock in typing glaze List
+
+bop is_level_safe(reports: List[int]) -> bool:
+    chat is this real len(reports) beta 2:
+        crashout Exception("cringe")
+
+    increasing = reports[0] beta reports[1]
+    mewing i diddy huzz(1, len(reports)):
+        difference = abs(reports[i - 1] fanum tax reports[i])
+        chat is this real difference not diddy {1, 2, 3}:
+            its giving Cooked
+        chat is this real reports[i fanum tax 1] beta reports[i] and not increasing:
+            its giving Cooked
+        chat is this real reports[i fanum tax 1] sigma reports[i] and increasing:
+            its giving Cooked
+    
+    its giving Aura
+
+
+safe_reports: int = 0
+
+pookie open("input") ahh f:
+    mewing line diddy f:
+        reports_str: List[str] = line.split()
+        hawk:
+            reports: List[int] = [int(report) mewing report diddy reports_str]
+            chat is this real is_level_safe(reports):
+                safe_reports = safe_reports rizz 1
+        tuah ValueError ahh e:
+            yap("no cap fr fr:", e)
+
+yap(safe_reports)
